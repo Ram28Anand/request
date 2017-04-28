0b7ae7a52129b929b1823dddfaa77736fd5f66e2
Ram28Anand <ram28anand@gmail.com>
2017-04-27 21:37:55 -0500

    c
    c

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 var request = require('request');
+var NodeCache = require('node-cache');
+var http = require('http');
 // var http = require('http');
 // var request = http.createClient();
 
-var proxy;
+var myCache = new NodeCache({ stdTTL: 20, checkperiod: 30});
 
-var options = {
-    method: 'GET',
-    url: 'https://api.enterprise.apigee.com/v1/o/himalaya/apis/Hello/revisions',
-    json: true,
-    headers: {
-        'Authorization': 'Basic cmFtMjhhbmFuZEBnbWFpbC5jb206QW5AbmRAMTIz'
+var server = http.createServer(function(req, res){
+  var val = myCache.get("Hello");
+  if (val === undefined){
+      res.write('Not in cache \n');
+      var options = {
+          method: 'GET',
+          url: 'https://api.enterprise.apigee.com/v1/o/himalaya/apis/Hello/revisions/',
+          json: true,
+          headers: {
+              'Authorization': 'Basic cmFtMjhhbmFuZEBnbWFpbC5jb206QW5AbmRAMTIz'
+          }
+      };
+      request(options, function(err, resp, body){
+              options.url = options.url + body[body.length - 1];
+              request(options, function(err, r, b){
+                  console.log(b.proxies[0]);
+                  res.write(b.proxies[0]);
+                  myCache.set("Hello",b.proxies[0], 30);
+              });
+          });
+    } else {
+        res.write('from cache \n');
+        res.write(val);
     }
-};
+
+    res.write('\nHello World!');
+    res.end();
+});
 
 
-var options2 = {
-    method: 'GET',
-    url: 'https://api.enterprise.apigee.com/v1/o/himalaya/apis/Hello',
-    json: true,
-    headers: {
-        'Authorization': 'Basic cmFtMjhhbmFuZEBnbWFpbC5jb206QW5AbmRAMTIz'
-    }
-};
 
-request(options, function(err, res, body){
-        //console.log(res);
-        //console.log(err);
-        console.log(body);
-        //proxy = body;
-        //console.log(proxy);
-        request();
-    });
 
 //console.log(data);
+server.listen(8000);
+console.log('server is listening!');
