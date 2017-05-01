58dff115d6ddbd24d081a50e81c6d49db451c499
Ram28Anand <ram28anand@gmail.com>
2017-04-30 20:20:34 -0500

    c
    c

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var request = require('request');
 var NodeCache = require('node-cache');
 var http = require('http');
-// var http = require('http');
-// var request = http.createClient();
 
+var base_path = 'https://api.enterprise.apigee.com/v1/o/himalaya/apis/Hello/revisions/';
 var myCache = new NodeCache({ stdTTL: 20, checkperiod: 30});
 
 var server = http.createServer(function(req, res){
@@ -12,28 +11,30 @@ var server = http.createServer(function(req, res){
       res.write('Not in cache \n');
       var options = {
           method: 'GET',
-          url: 'https://api.enterprise.apigee.com/v1/o/himalaya/apis/Hello/revisions/',
+          url: base_path,
           json: true,
           headers: {
               'Authorization': 'Basic cmFtMjhhbmFuZEBnbWFpbC5jb206QW5AbmRAMTIz'
           }
       };
       request(options, function(err, resp, body){
-              options.url = options.url + body[body.length - 1];
+              options.url = options.url + body[body.length - 1]+'/resources/jsc/auth.js';
               request(options, function(err, r, b){
-                  console.log(b.proxies[0]);
-                  res.write(b.proxies[0]);
-                  myCache.set("Hello",b.proxies[0], 30);
+                  console.log(b.authlevel);
+                  myCache.set("Hello",b.authlevel, 30);
               });
           });
     } else {
-        res.write('from cache \n');
         res.write(val);
+        res.write('\nfrom cache \n');
     }
 
     res.write('\nHello World!');
     res.end();
 });
+
+server.listen(8000);
+console.log('server is listening!');
 
 
 
